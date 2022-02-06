d11a1009a2a6fb4b6c3c0f4a2fb99e230d9d6001
VladimirGromyko <vl-mailbox@mail.ru>
2022-02-06 12:41:27 +0300

    Credit calculator - correct small mistake in api request

--- src/api/rate-api.ts
+++ src/api/rate-api.ts
@@ -1,8 +1,8 @@
 const today = new Date()
 const todayYear = today.getFullYear()
 const todayMonth = today.getMonth()
-const todayDay = today.getDay()
-const todayStr = todayYear.toString() + '-' + todayMonth.toString() + '-' + todayDay
+const todayDay = today.getDate()
+const todayStr = todayYear.toString() + '-' + todayMonth.toString() + '-' + todayDay.toString()
 const apiUrl = `https://www.nbrb.by/api/refinancingrate?ondate=${todayStr}`
 
 export const rateApi = {
